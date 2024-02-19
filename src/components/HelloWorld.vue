a0f44d39b5501a45d88f8eb1efefb02255cbf90c
Vokhidjon12 <vipkeyl001@gmail.com>
2024-02-20 02:20:40 +0500

    firt bug

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,90 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-pwa" target="_blank" rel="noopener">pwa</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router" target="_blank" rel="noopener">router</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div>
+        <div class="loader">
+        <span>M</span>
+        <span>A</span>
+        <span>R</span>
+        <span>J</span>
+        <span>O</span>
+        <span>N</span>
+        <span>A</span>
+        <span><img src="@/assets/heart.png" alt="img"></span>
+    </div>
   </div>
 </template>
 
-<script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
-</script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+*{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+.loader{
+  position: absolute;
+  top: 50%;
+  left: 50%;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+body{
+    font-family: sans-serif;
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: #111;
 }
-a {
-  color: #42b983;
+.loader span {
+    font-size: 60px;
+    text-transform: uppercase;
+    color: aqua;
+    font-weight: bold;
+    animation: animate 5s ease-in-out infinite;
+}
+.loader span img {
+    max-width: 50px;
+}
+
+@keyframes animate{
+    10%{color: yellow ;}
+    20%{color: green ;}
+    30%{color: pink ;}
+    40%{color: blue ;}
+    50%{color: orange ;}
+    60%{color: darkred ;}
+    70%{color: darkblue ;}
+    80%{color: yellowgreen ;}
+    90%{color: orangered ;}
+    100%{color: aqua ;}
+}
+
+
+.loader span:nth-child(1){
+    animation-delay: 0.8s;
+}
+.loader span:nth-child(2){
+    animation-delay: 1s;
+}
+.loader span:nth-child(3){
+    animation-delay: 1.2s;
+}
+.loader span:nth-child(4){
+    animation-delay: 1.4s;
+}
+.loader span:nth-child(5){
+    animation-delay: 1.6s;
+}
+.loader span:nth-child(6){
+    animation-delay: 1.8s;
+}
+.loader span:nth-child(7){
+    animation-delay: 2s;
+}
+.loader span:nth-child(8){
+    animation-delay: 2.2s;
+}
+.loader span:nth-child(9){
+    animation-delay: 2.4s;
+}
+.loader span:nth-child(10){
+    animation-delay: 2.6s;
 }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,15 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
+    
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
+<style scoped>
+.home{
+  background: #000;
+}
+</style>
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
